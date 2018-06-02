2f6c84925672dbcdb9c0aff527c4dcf8fc772fff
Tomoyoshi Yamamoto <tomoyoshi.usa@gmail.com>
2018-06-02 18:35:28 +0900

    add more files and more updates to get closer to beta

--- js/ai/ai.brain.js
+++ js/ai/ai.brain.js
@@ -1,52 +1,59 @@
 var aiBrain = {
-  request : function () {
-    var usrInputS = usrInput.value;
-    usrInput.value = "";
-    usrInputDisplay.innerHTML = usrInputS;
-    aiStatsEle.innerHTML = "thinking";
-    var usrInputA = usrInputS.split(" ");
-    //create the friendly grammer pattern! (excited)
-    var grammerPatternSubmit = [];
-    for(i=0;i<usrInput.length;i++){
-      var analysisWord = usrInput[i];
-      if(aiDict.verb.indexOf(analysisWord)!=-1){
-        grammerPatternSubmit.push(2);
-      }else if(aiDict.verbPP.indexOf(analysisWord)!=-1){
-        grammerPatternSubmit.push(3);
-      }else if(aiDict.verbSP.indexOf(analysisWord)!=-1){
-        grammerPatternSubmit.push(4);
-      }else if(aiDict.adjective.indexOf(analysisWord)!=-1){
-        grammerPatternSubmit.push(5);
-      }else if(aiDict.adverb.indexOf(analysisWord)!=-1){
-        grammerPatternSubmit.push(6);
-      }else if(aiDict.sPronoun.indexOf(analysisWord)!=-1){
-        grammerPatternSubmit.push(7);
-      }else if(aiDict.oPronoun.indexOf(analysisWord)!=-1){
-        grammerPatternSubmit.push(8);
-      }else if(aiDict.pPronoun.indexOf(analysisWord)!=-1){
-        grammerPatternSubmit.push(9);
-      }else if(aiDict.preposition.indexOf(analysisWord)!=-1){
-        grammerPatternSubmit.push("!");
-      }else if(aiDict.conjunction.indexOf(analysisWord)!=-1){
-        grammerPatternSubmit.push("@");
-      }else if(aiDict.determiner.indexOf(analysisWord)!=-1){
-        grammerPatternSubmit.push("#");
-      }else if(aiDict.exclamation.indexOf(analysisWord)!=-1){
-        grammerPatternSubmit.push("$");
-      }else if(aiDict.verb.indexOf(analysisWord)!=-1&&aiDict.verbPP.indexOf(analysisWord)!=-1&&aiDict.verbSP.indexOf(analysisWord)!=-1&&aiDict.adjective.indexOf(analysisWord)!=-1&&aiDict.adverb.indexOf(analysisWord)!=-1&&aiDict.sPronoun.indexOf(analysisWord)!=-1&&aiDict.oPronoun.indexOf(analysisWord)!=-1&&aiDict.pPronoun.indexOf(analysisWord)!=-1&&aiDict.preposition.indexOf(analysisWord)!=-1&&aiDict.conjunction.indexOf(analysisWord)!=-1&&aiDict.determiner.indexOf(analysisWord)!=-1&&aiDict.exclamation.indexOf(analysisWord)!=-1) {
-        grammerPatternSubmit.push("%");
-      }
-      var gramNetowrk = this.decodeGramPat(grammerPatternSubmit);
-      var product = this.createCode();
-    }
-  },
-  decodeGramPat : function (patternA) {
-    var patternS = patternA.join("");
-  },
-  learnKeys : function () {
+	request: function() {
+		var usrInputS = usrInput.value.toLowerCase();
+		usrInput.value = "";
+		usrInputDisplay.innerHTML = usrInputS;
+		aiStatsEle.innerHTML = "thinking";
+		var usrInputA = usrInputS.split(" ");
+		//create the friendly grammer pattern! (excited)
+		var grammerPatternSubmit = [];
+		for (i = 0; i < usrInputA.length; i++) {
+			var analysisWord = usrInputA[i];
+			console.log(analysisWord);
+			if (aiDict.verb.indexOf(analysisWord) != -1) {
+				grammerPatternSubmit.push(2);
+			} else if (aiDict.verbPP.indexOf(analysisWord) != -1) {
+				grammerPatternSubmit.push(3);
+			} else if (aiDict.verbSP.indexOf(analysisWord) != -1) {
+				grammerPatternSubmit.push(4);
+			} else if (aiDict.adjective.indexOf(analysisWord) != -1) {
+				grammerPatternSubmit.push(5);
+			} else if (aiDict.adverb.indexOf(analysisWord) != -1) {
+				grammerPatternSubmit.push(6);
+			} else if (aiDict.sPronoun.indexOf(analysisWord) != -1) {
+				grammerPatternSubmit.push(7);
+			} else if (aiDict.oPronoun.indexOf(analysisWord) != -1) {
+				grammerPatternSubmit.push(8);
+			} else if (aiDict.pPronoun.indexOf(analysisWord) != -1) {
+				grammerPatternSubmit.push(9);
+			} else if (aiDict.preposition.indexOf(analysisWord) != -1) {
+				grammerPatternSubmit.push("!");
+			} else if (aiDict.conjunction.indexOf(analysisWord) != -1) {
+				grammerPatternSubmit.push("@");
+			} else if (aiDict.determiner.indexOf(analysisWord) != -1) {
+				grammerPatternSubmit.push("#");
+			} else if (aiDict.exclamation.indexOf(analysisWord) != -1) {
+				grammerPatternSubmit.push("$");
+			} else if (aiDict.verb.indexOf(analysisWord) == -1 && aiDict.verbPP.indexOf(analysisWord) == -1 && aiDict.verbSP.indexOf(analysisWord) == -1 && aiDict.adjective.indexOf(analysisWord) == -1 && aiDict.adverb.indexOf(analysisWord) == -1 && aiDict.sPronoun.indexOf(analysisWord) == -1 && aiDict.oPronoun.indexOf(analysisWord) == -1 && aiDict.pPronoun.indexOf(analysisWord) == -1 && aiDict.preposition.indexOf(analysisWord) == -1 && aiDict.conjunction.indexOf(analysisWord) == -1 && aiDict.determiner.indexOf(analysisWord) == -1 && aiDict.exclamation.indexOf(analysisWord) == -1) {
+				grammerPatternSubmit.push(1);
+			}
+			console.log(grammerPatternSubmit);
+			var gramNetwork = this.decodeGramPat(grammerPatternSubmit);
+      console.log(gramNetwork);
+		}
+	},
+	decodeGramPat: function(patternA) {
+		for (o = 0; o < patternA.length; o++) {
+			var configPattern = patternA[o];
+			if (configPattern == 1) {
 
-  },
-  createCode : function (networkAuth) {
+			}
+		}
+	},
+	learnKeys: function() {
 
-  }
+	},
+	createCode: function(networkAuth) {
+
+	}
 }
--- js/ui/elementManager.js
+++ js/ui/elementManager.js
@@ -2,3 +2,13 @@
 var usrInput = document.getElementById("usrInput");
 var usrInputDisplay = document.getElementById("usrInputDisplay");
 var aiStatsEle = document.getElementById("aiStatsEle");
+var submitBtn = document.getElementById("submitBtn");
+
+submitBtn.disabled = true;
+usrInput.onkeyup = function () {
+  if(usrInput.value.length == 0){
+    submitBtn.disabled = true;
+  } else {
+    submitBtn.disabled = false;
+  }
+}
